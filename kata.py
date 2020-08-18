077401cbfe9afcde928db1ef3d4712f439f2b78c
Levi <50143244+levinol@users.noreply.github.com>
2020-08-18 04:08:11 +0300

    Modulation big update

--- kata.py
+++ kata.py
@@ -3,16 +3,25 @@ def solve_mine(map, n):
     mine = [i.split(' ') for i in map.split('\n')]
     field = MineSweeper(mine, n)
     #compare last map with new map
-    for i in range(10):
+    i = 0
+    counter = 0
+    while (field.status_dict['n'] and counter <2):
         print("Iteration: ", i)
         st1 = field.get_map()
         field.maybe_solve()
         st2 = field.get_map()
         if st1 == st2:
             print('we stuck')
+            counter += 1
             field.modulate_solve()
+        else:
+            counter = 0
         print(field)
-    return 0
+        i +=1
+    if counter ==2:
+        return '?'
+    else:
+        return st2
 
 class MineSweeper():
     def __init__(self, map, n):
@@ -100,6 +109,7 @@ class MineSweeper():
                     except IndexError:
                         continue
         return count_x, count_h, near_h
+    
                     
     def maybe_solve(self):
         numbers = self.status_dict['n'].copy()
@@ -122,18 +132,148 @@ class MineSweeper():
                 self.status_dict['u'].append((row,column))
         return 0
 
+    
+
+
+    @staticmethod
+    def maybe_x(map, stat_dict, number, bomb):
+        maybe_x_arr = [bomb]
+        fake_opened_arr = []
+        #check neighb
+        #while?
+        print(self.intersection(map, number, bomb))
+
+        numbers = []
+        for row, column in numbers:
+            #check neighb
+            c_x, c_h, near = self.x_fake(map, row, column)
+            if c_x + c_h == int(map[row][column]):
+                for r, c in near:
+                    maybe_x_arr.append((r,c))
+                #remove from numbers
+            elif c_x == int(self.map[row][column]):
+                for r, c in near:
+                    self.map[r][c] = open(r,c)
+                    fake_opened_arr.append((r,c))
+                #remove from numbers
+            elif c_x + c_h > int(map[row][column]):
+                #развилка
+                print('Код красный код красный')
+        return 0
+
     def modulate_solve(self):
         numbers = self.status_dict['n'].copy()
         temp_dict ={}
         for row, column in numbers:
             c_x, c_h, near = self.x(self.map, row, column)
-            #print("Im in ", row, column)
-            #print(int(self.map[row][column]) - c_x, 'bombs near')
-            #print(near)
             temp_dict[(row,column)] = (int(self.map[row][column]) - c_x, len(near), near)
         temp_order_arr = sorted(temp_dict, key=lambda k: (temp_dict[k][1], temp_dict[k][0]))
         for i in temp_order_arr:
-            print(temp_dict[i])
+            for j in temp_dict[i][2]:
+                #near maybe bombs
+                print(i, '->', j)
+                maybe_x, fo_arr, flag = modulate_x(self.map, self.status_dict, i, j)
+                if flag == 1:
+                    self.map[j[0]][j[1]] = open(j[0],j[1])
+                    print('I opened')
+                    self.status_dict['b'].remove(j)
+                    self.status_dict['n'].append(j)
+                    break
+                elif flag == 2:
+                    print('Развилка хуле')
+                    continue
+                else:
+                    #суммировать maybe_x и fo_arr в словарь
+                    continue
+            if flag == 1:
+                break
+            #arrays of temp_open and temp_x
+            #counter
+        
+
+
+def modulate_x(map, stat_dict, number, bomb):
+    maybe_x_arr = [bomb]
+    fake_opened_arr = []
+    #check neighb
+    #while?
+    numbers = intersection(map, number, bomb)
+    flag = 0
+    while numbers and flag==0:
+        numers_for_iteration = numbers.copy()
+        for row, column in numers_for_iteration:
+            #check neighb
+            c_x, c_h, near = x_fake(map, maybe_x_arr, fake_opened_arr, row, column)
+            if c_x + c_h == int(map[row][column]):
+                for r, c in near:
+                    maybe_x_arr.append((r,c))
+                    numbers.update(intersection(map,(row,column), (r,c)))
+                #remove from numbers
+                numbers.remove((row,column))
+            elif c_x == int(map[row][column]):
+                for r, c in near:
+                    fake_opened_arr.append((r,c))
+                    numbers.update(intersection(map,(row,column), (r,c)))
+                #remove from numbers
+                numbers.remove((row,column))
+            elif c_x + c_h > int(map[row][column]):
+                #развилка
+                print('Код красный код красный')
+                flag = 2
+            elif c_h == 0 and c_x:
+                print('We are in fucked scenario')
+                flag = 1
+    return maybe_x_arr, fake_opened_arr, flag
+
+
+def x_fake(map, bomb_arr, fake_arr, n, m):
+    near_h = []
+    count_x = 0
+    count_h = 0
+    for i in range(n-1, n+2):
+        for j in range(m-1, m+2):
+            if i == n and j == m:
+                continue
+            elif i < 0 or j < 0:
+                continue
+            else:
+                try:
+                    if map[i][j] == '?':
+                        if (i,j) in fake_arr:
+                            continue
+                        elif (i,j) in bomb_arr:
+                            count_x +=1
+                        else:
+                            count_h += 1
+                            near_h.append((i,j))
+                    elif map[i][j] == 'x':
+                        count_x +=1
+                except IndexError:
+                    continue
+    return count_x, count_h, near_h
+
+def intersection(map, number, bomb):
+    return near_n(map, number)&near_n(map, bomb)
+
+def near_n(map, cell):
+    n = cell[0]
+    m = cell[1]
+    near_n = set()
+    for i in range(n-1, n+2):
+        for j in range(m-1, m+2):
+            if i == n and j == m:
+                continue
+            elif i < 0 or j < 0:
+                continue
+            else:
+                try:
+                    if str(map[i][j]) in '012345678':
+                        near_n.add((i,j))
+                except IndexError:
+                    continue
+    return near_n
+
+
 
 gamemap = """
 ? ? ? ? ? ?
