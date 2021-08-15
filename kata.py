46039845683040f76400ebf0512bcbe9506e7053
Levi <50143244+levinol@users.noreply.github.com>
2021-08-15 23:48:22 +0300

    Update kata.py

--- kata.py
+++ kata.py
@@ -399,5 +399,5 @@ if __name__ == "__main__":
     print('Receiving cmd args: ',sys.argv[1])
     print('Lookin for files in directory ', os.listdir('.'))
     with open(sys.argv[1], 'r') as f:
-        gamemap, result = json.load(f, strict=False)
-    print(solve_mine(gamemap, 4))
+        gamemap, result, mine_count = json.load(f, strict=False)
+    print(solve_mine(gamemap, mine_count))
